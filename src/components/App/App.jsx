79b7ce2f9b5c7ded9bd6b9eccac5763b4c994ee4
Raymond Terry <raymondt1234@gmail.com>
2025-12-22 17:20:09 -0700

    setup react using vite and add the background image

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__content">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
